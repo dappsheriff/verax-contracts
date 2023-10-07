ebb0d3bbe46808c0599bb5f180146f14988f49d7
Yerassyl Zhanymkanov <zhanymkanov@gmail.com>
2023-10-07 22:42:06 +0600

    Upgrade to OZ 5.0.0

--- src/DappSheriff.sol
+++ src/DappSheriff.sol
@@ -1,34 +1,24 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.21;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract DappSheriff is ERC721, ERC721URIStorage, Ownable {
-    using Counters for Counters.Counter;
-
-    Counters.Counter private _tokenIdCounter;
+    uint256 private _nextTokenId;
     uint256 public price = 0.001 ether;
 
     event PriceChanged(uint256 newPrice);
 
-    constructor() ERC721("DappSheriff", "DPS") {}
+    constructor(address initialOwner) ERC721("DappSheriff", "DPS") Ownable(initialOwner) {}
 
     function mint(address to, string memory uri) external payable {
         require(msg.value == price, "Wrong ETH value");
 
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
-
+        uint256 tokenId = _nextTokenId++;
         _mint(to, tokenId);
-
         _setTokenURI(tokenId, uri);
-    }
-
-    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
-        super._burn(tokenId);
     }
 
     function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
